2d14f354d0c77fd039a4e4681bcc3bb77d2ad40a
Mauricio Rivera <mauricio.rivera1993@yahoo.com>
2020-09-25 22:32:08 -0700

    Updates

---
+++ precache-manifest.3e4df8b9b84f23b0874068c7a1d3f474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d12e0e04c2a65d10da60d0dac83c27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53104d06c449fc1ca26e",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c9faf972b34e4f2e4b64",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53104d06c449fc1ca26e",
+    "url": "/static/js/2.8abdb473.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8abdb473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9faf972b34e4f2e4b64",
+    "url": "/static/js/main.0bf302f1.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  }
+]);
