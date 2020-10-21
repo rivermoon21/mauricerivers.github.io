0bb91a1c50c95b2821eaa2ca5ba69e82841690c7
Mauricio Rivera <mauricio.rivera1993@yahoo.com>
2020-10-20 17:34:10 -0700

    Deleted socials page and added it in the footer

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -10,7 +10,6 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
-import SocialsPage from './pages/SocialsPage';
 
 class App extends React.Component {
 
