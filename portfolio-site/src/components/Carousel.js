ee820fe352b763cd9c55b773e737be054d1cb398
rivermoon21 <mrivermoon@protonmail.com>
2021-03-31 16:32:02 -0700

    Updated page links

--- portfolio-site/src/components/Carousel.js
+++ portfolio-site/src/components/Carousel.js
@@ -10,79 +10,85 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 class Carousel extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      items: [
-        {
-          id:0,
-          title: 'Personal',
-          subTitle: 'GitHub',
-          imgSrc: avatar,
-          link: 'https://stackoverflow.com/',
-          selected: false
-        },
-        {
-          id:1,
-          title: 'Spacecraft',
-          subTitle: 'CubeSats',
-          imgSrc: satellite,
-          link: 'https://youtube.com',
-          selected: false
-        },
-        {
-          id:2,
-          title: 'Blockchain',
-          subTitle: 'Smart Contracts',
-          imgSrc: blockchain,
-          link: 'https://github.com',
-          selected: false
-        },
-        {
-          id:3,
-          title: 'Hardware',
-          subTitle: 'Embedded Projects',
-          imgSrc: chip,
-          link: 'https://www.adafruit.com/',
-          selected: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [{
+                    id: 0,
+                    title: 'Personal',
+                    subTitle: 'GitHub',
+                    imgSrc: avatar,
+                    link: 'https://github.com/rivermoon21',
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: 'Spacecraft',
+                    subTitle: 'CubeSats',
+                    imgSrc: satellite,
+                    link: 'https://www.cubesat.org',
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: 'Blockchain',
+                    subTitle: 'Smart Contracts',
+                    imgSrc: blockchain,
+                    link: 'https://www.trufflesuite.com',
+                    selected: false
+                },
+                {
+                    id: 3,
+                    title: 'Hardware',
+                    subTitle: 'Embedded Projects',
+                    imgSrc: chip,
+                    link: 'https://www.adafruit.com',
+                    selected: false
+                }
+            ]
         }
-      ]
     }
-  }
 
-  handlerCardClick = (id, card) => {
-    let items = [...this.state.items];
+    handlerCardClick = (id, card) => {
+        let items = [...this.state.items];
 
-    items[id].selected = items[id].selected ? false : true;
-    items.forEach(item => {
-      if(item.id !== id){
-        item.selected = false;
-      }
-    });
+        items[id].selected = items[id].selected ? false : true;
+        items.forEach(item => {
+            if (item.id !== id) {
+                item.selected = false;
+            }
+        });
 
-    this.setState({
-      items
-    });
+        this.setState({
+            items
+        });
 
-  }
+    }
 
-  makeItems = (items) => {
-    return items.map(item => {
-      return <Card item={item} click={(e => this.handlerCardClick(item.id, e))} key={item.id} />
-    })
-  }
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item = { item }
+            click = {
+                (e => this.handlerCardClick(item.id, e))
+            }
+            key = { item.id }
+            />
+        })
+    }
 
 
-  render() {
-    return(
-      <Container fluid={true}>
-        <Row className="justify-content-center">
-          {this.makeItems(this.state.items)}
-        </Row>
+    render() {
+        return ( <
+            Container fluid = { true } >
 
-      </Container>
-    );
-  }
+            <
+            Row className = "justify-content-center" > { this.makeItems(this.state.items) } <
+            /Row>
+
+            <
+            /Container>
+        );
+    }
 
 }
 
--- portfolio-site/src/components/Footer.js
+++ portfolio-site/src/components/Footer.js
@@ -8,31 +8,74 @@ import Modal from "../components/Modal";
 
 function Footer() {
 
-    return (
-      <footer className="mt-5">
-        <Container fluid={true} >
-        <Col className="border-top justify-content-between p-3" >
+    return ( <
+        footer className = "mt-5" >
+        <
+        Container fluid = { true } >
+        <
+        Col className = "border-top justify-content-between p-3" >
 
-          <Row className="justify-content-center" style={{ fontFamily: "Kaushan Script, cursive", color: "#c1b" }} >
-            <Col lg="1" sm="4" xs="4" >
-              <SocialIcon url="https://github.com/rivermoon21" target="_blank" rel="noopener noreferrer" bgColor="#e600e6" style={{ height: 50, width: 50 }} />
-            </Col>
+        <
+        Row className = "justify-content-center"
+        style = {
+            { fontFamily: "Kaushan Script, cursive", color: "#c1b" }
+        } >
+        <
+        Col lg = "1"
+        sm = "4"
+        xs = "4" >
+        <
+        SocialIcon url = "https://github.com/rivermoon21"
+        target = "_blank"
+        rel = "noopener noreferrer"
+        bgColor = "#e600e6"
+        style = {
+            { height: 50, width: 50 }
+        }
+        /> < /
+        Col >
 
-            <Col lg="1" sm="4" xs="4" >
-              <SocialIcon url="https://www.linkedin.com/in/mrivera93/" target="_blank" rel="noopener noreferrer" bgColor="#e600e6" style={{ height: 50, width: 50 }} />
-            </Col>
+        <
+        Col lg = "1"
+        sm = "4"
+        xs = "4" >
+        <
+        SocialIcon url = "https://www.linkedin.com/in/mrivera93/"
+        target = "_blank"
+        rel = "noopener noreferrer"
+        bgColor = "#e600e6"
+        style = {
+            { height: 50, width: 50 }
+        }
+        /> < /
+        Col >
 
-            <Col lg="1" sm="4" xs="4" >
-              <SocialIcon url="https://twitter.com/rivermoon21" target="_blank" rel="noopener noreferrer" bgColor="#e600e6" style={{ height: 50, width: 50 }} />
-            </Col>
-          </Row>
+        <
+        Col lg = "1"
+        sm = "4"
+        xs = "4" >
+        <
+        SocialIcon url = "https://twitter.com/lunariveram"
+        target = "_blank"
+        rel = "noopener noreferrer"
+        bgColor = "#e600e6"
+        style = {
+            { height: 50, width: 50 }
+        }
+        /> < /
+        Col > <
+        /Row>
 
-            <Row className="p-3 justify-content-center" md={3} style={{ fontFamily: "Andale Mono, monospace", fontSize: 15, color: "#c1b" }}>
-              © 2020 Developed by Mauricio. All Rights Reserved.
-            </Row>
-        </Col>
-        </Container>
-      </footer>
+        <
+        Row className = "p-3 justify-content-center"
+        md = { 3 }
+        style = {
+            { fontFamily: "Andale Mono, monospace", fontSize: 15, color: "#c1b" }
+        } > ©2020 Developed by Mauricio.All Rights Reserved. <
+        /Row> < /
+        Col > <
+        /Container> < /
+        footer >
     );
 }
 
